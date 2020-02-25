758a5e11e917c17bc22d6ad2f71424a2fed25ab6
Rhaisa Alves <rhaisalouiseadm@gmail.com>
2020-02-25 17:57:10 +0100

    Criate a page next-js

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header> My page</header>
+      <main>
+        <h1></h1>
+      </main>
+    </>
+  );
+}

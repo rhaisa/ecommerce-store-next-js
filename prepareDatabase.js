8cb47f6ec506d6d02db49614b857c0e0cadb1edd
Rhaisa Alves <rhaisalouiseadm@gmail.com>
2020-03-23 12:00:40 +0100

    Trying to use Heroku with rejectUnauthorized

---
+++ prepareDatabase.js
@@ -0,0 +1,19 @@
+const postgres = require('postgres');
+const sql = process.env.DATABASE_URL
+  ? postgres(process.env.DATABASE_URL, { ssl: { rejectUnauthorized: false } })
+  : postgres();
+
+async function createTable() {
+  await sql`
+  CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR  NOT NULL,
+    description TEXT NOT NULL,
+    short_description TEXT NOT NULL,
+    price DECIMAL NOT NULL,
+    image VARCHAR  NOT NULL,
+    instagram_url VARCHAR  NOT NULL
+  );
+  `;
+}
+createTable();

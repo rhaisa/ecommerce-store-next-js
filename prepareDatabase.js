f9c743bd2135eadbbcc8022699a756d13ea33222
Rhaisa Alves <rhaisalouiseadm@gmail.com>
2020-03-23 12:16:21 +0100

    Using "dotenv"

--- prepareDatabase.js
+++ prepareDatabase.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const postgres = require('postgres');
 const sql = process.env.DATABASE_URL
   ? postgres(process.env.DATABASE_URL, { ssl: { rejectUnauthorized: false } })

dfc81d0e0b09ae10811df933aecc1f62f3e70004
Rhaisa Alves <rhaisalouiseadm@gmail.com>
2020-03-23 12:26:48 +0100

    Debugging

--- prepareDatabase.js
+++ prepareDatabase.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
+// require('dotenv').config();
 const postgres = require('postgres');
+
 const sql = process.env.DATABASE_URL
   ? postgres(process.env.DATABASE_URL, { ssl: { rejectUnauthorized: false } })
   : postgres();
